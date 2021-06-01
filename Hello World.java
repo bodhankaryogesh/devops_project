c5435c8577026d0b60cc067938a552c653f874b5
unknown <bodhankaryogesh@gmail.com>
2021-06-02 00:33:07 +0530

    class DemoClass {   public static void main (String a[])   {     System.out.println("Hello World");     System.out.println(8/0);     System.out.println("Completed");   } } class Demo1{
    }

---
+++ Hello World.java
@@ -0,0 +1,11 @@
+class DemoClass {
+  public static void main (String a[])
+  {
+    System.out.println("Hello World");
+    System.out.println(8/0);
+    System.out.println("Completed");
+  }
+}
+class Demo1{
+
+}
